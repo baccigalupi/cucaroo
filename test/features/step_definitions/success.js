2f83986fbc80982e32feba634dd611919e38f9b8
Kane Baccigalupi <baccigalupi@gmail.com>
2017-06-18 09:10:27 -0700

    Adds manually run feature to make sure everything looks right

---
+++ test/features/step_definitions/success.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const assert = require('assert');
+
+module.exports = function(world) {
+  world.given('all step definitions are defined', function(done) {
+    done();
+  });
+
+  world.when('I run the feature', function(done) {
+    done();
+  });
+
+  world.then('I should see all tests in green', function(done) {
+    done();
+  });
+
+  world.and('the exit code should be 0', function(done) {
+    assert.equal(world.errors, 0);
+    done();
+  });
+};
---
+++ test/features/success.feature
@@ -0,0 +1,10 @@
+Feature: Successful feature runs
+  As a BDD developer
+  I want to implement and run cucumber tests
+  So that I have regression testing, and a starting place for talking with product people
+
+  Scenario: All is good
+    Given all step definitions are defined
+    When I run the feature
+    Then I should see all tests in green
+    And the exit code should be 0
