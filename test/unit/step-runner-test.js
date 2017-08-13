f6db52dabc189410c3c1fe738807bd354c0d2401
Kane Baccigalupi <baccigalupi@gmail.com>
2017-08-13 12:55:14 -0700

    When running a step receives a non-error it emits a failure
    Both `horseman` and `nightmare` have chaning that pass along
    unexpected things to the `done` callback for each step. To allow
    developers to debug, the code should not fail trying to parse the error
    as an error!

--- test/unit/step-runner-test.js
+++ test/unit/step-runner-test.js
@@ -114,4 +114,35 @@ describe('StepRunner', function() {
       done();
     });
   });
+
+  describe('#emit(err)', function() {
+    let events;
+
+    beforeEach(function() {
+      events = [];
+      stepValue.onAny(function(event) {
+        events.push(event);
+      });
+    });
+
+    it('when there is no error, it emits a \'pass\' on the step', function() {
+      runner.emit();
+      assert.equal(events[0], 'pass');
+    });
+
+    it('when there is a pending error, it emits a \'pending\' on the step', function() {
+      runner.emit(new Error('Pending'));
+      assert.equal(events[0], 'pending');
+    });
+
+    it('when there is a different error, it emits a \'fail\' on the step', function() {
+      runner.emit(new Error('Oh crap!'));
+      assert.equal(events[0], 'fail');
+    });
+
+    it('when it receives a non-error value, it emits a a \'fail\' on the step', function() {
+      runner.emit('some random string');
+      assert.equal(events[0], 'fail');
+    });
+  });
 });
